7061709caa6d620deed6fe0fe7407fa26556241b
Cody C <50791984+codyc1515@users.noreply.github.com>
2025-02-19 21:22:13 +1300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rtl_fm -f 157.925M -p -4 -s 22050 | \
+rtl_fm -f 157.95M -p -4 -s 22050 | \
 multimon-ng -q -b1 -c -t raw -a POCSAG512 -a POCSAG1200 -a POCSAG2400 -a FLEX -f alpha /dev/stdin | \
 node reader.js
 
