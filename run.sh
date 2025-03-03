86c6e2a0e6f79e930215dc4ceb68c40b72dd2859
Cody C <50791984+codyc1515@users.noreply.github.com>
2025-03-03 22:16:06 +1300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-rtl_fm -f 157.95M -p 27 -s 22050 | \
+FREQ="${FREQ:-157.95M}"  # Default to 157.95M if FREQ is not set
+
+rtl_fm -f "$FREQ" -p 27 -s 22050 | \
 multimon-ng -q -b1 -c -t raw -a POCSAG512 -a POCSAG1200 -a POCSAG2400 -a FLEX -f alpha /dev/stdin | \
 node reader.js
-
-#exec tail -f /dev/null
